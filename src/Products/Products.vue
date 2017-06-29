57f57c41802122258704fcee2ba7aa1efba9c81f
Henrikh Abajyan <h.abajyan@gmail.com>
2017-06-29 13:04:27 +0400

    Adding product rating and some stile corection

--- src/Products/Products.vue
+++ src/Products/Products.vue
@@ -5,187 +5,165 @@
 
     <div class="ProductsSection">
       <div class="Container">
-        <h1> Our Products </h1>
+        <h1 class="SectionTitle"> Our Products </h1>
 
-        <h1 class="Title">Bsiness Suitcases</h1>
+
+        <h1 class="Title">Business Suitcases</h1>
+
         <div class="row">
-
           <div class="col-xs-12 col-sm-6 col-md-3">
-            <div class="Product">
-              <div class="img" style="background-image: url('http://g03.a.alicdn.com/kf/HTB1SuMSKVXXXXcRaXXXq6xXFXXXz/2015-Business-font-b-Leather-b-font-font-b-Trolley-b-font-Luggage-Vintage-Suitcase-Board.jpg')"></div>
-              <div class="row">
-                <div class="col-xs ProductCol">
-                  <h3 class="Name">1 Suitcase</h3>
-                </div>
-                <div class="col-xs PriceCol">
-                  <div class="Price">63$</div>
-                </div>
-              </div>
-            </div>
+            <Product
+              name="1 Suitcase"
+              rating="3"
+              price="63"
+              img="http://g03.a.alicdn.com/kf/HTB1SuMSKVXXXXcRaXXXq6xXFXXXz/2015-Business-font-b-Leather-b-font-font-b-Trolley-b-font-Luggage-Vintage-Suitcase-Board.jpg">
+            </Product>
           </div>
 
           <div class="col-xs-12 col-sm-6 col-md-3">
-            <div class="Product">
-              <div class="img" style="background-image: url('https://images-na.ssl-images-amazon.com/images/I/41Zdz+bjYRL._AC_US218_.jpg')"></div>
-              <div class="row">
-                <div class="col-xs ProductCol">
-                  <h3 class="Name">2 Suitcase</h3>
-                </div>
-                <div class="col-xs PriceCol">
-                  <div class="Price">88$</div>
-                </div>
-              </div>
-            </div>
+            <Product
+              name="2 Suitcase"
+              rating="2"
+              price="88"
+              img="https://images-na.ssl-images-amazon.com/images/I/41Zdz+bjYRL._AC_US218_.jpg">
+            </Product>
           </div>
 
           <div class="col-xs-12 col-sm-6 col-md-3">
-            <div class="Product">
-              <div class="img" style="background-image: url('https://royalbag.com.ua/image/cache/data/Issa_Hara/B14C/B14C_1-300x300.jpg')"></div>
-              <div class="row">
-                <div class="col-xs ProductCol">
-                  <h3 class="Name">3 Suitcase</h3>
-                </div>
-                <div class="col-xs PriceCol">
-                  <div class="Price">40$</div>
-                </div>
-              </div>
-            </div>
+            <Product
+              name="3 Suitcase"
+              rating="4"
+              price="40"
+              img="https://royalbag.com.ua/image/cache/data/Issa_Hara/B14C/B14C_1-300x300.jpg">
+            </Product>
           </div>
 
-
           <div class="col-xs-12 col-sm-6 col-md-3">
-            <div class="Product">
-              <div class="img" style="background-image: url('http://thumbs4.ebaystatic.com/d/l225/m/mGrn9a-WhUeXASK3lph4BxQ.jpg')"></div>
-              <div class="row">
-                <div class="col-xs ProductCol">
-                  <h3 class="Name">4 Suitcase</h3>
-                </div>
-                <div class="col-xs PriceCol">
-                  <div class="Price">10$</div>
-                </div>
-              </div>
-            </div>
+            <Product
+              name="4 Suitcase"
+              rating="1"
+              price="78"
+              img="http://thumbs4.ebaystatic.com/d/l225/m/mGrn9a-WhUeXASK3lph4BxQ.jpg">
+            </Product>
           </div>
 
           <div class="col-xs-12 col-sm-12 col-md-6">
-            <div class="Top Product">
-              <div class="img" style="background-image: url('http://images.saddlebackleather.com/image/upload/w_1280,h_1280,c_fill/Mont_Blanc/SLC/Products/01-60-0007-LG-CH-03.jpg')"></div>
-              <div class="row">
-                <div class="col-xs ProductCol">
-                  <h3 class="Name">5 Suitcase</h3>
-                </div>
-                <div class="col-xs PriceCol">
-                  <div class="Price">99$</div>
-                </div>
-              </div>
-            </div>
+            <Product
+              class="Top"
+              name="5 Suitcase"
+              rating="5"
+              price="99"
+              img="http://images.saddlebackleather.com/image/upload/w_1280,h_1280,c_fill/Mont_Blanc/SLC/Products/01-60-0007-LG-CH-03.jpg">
+            </Product>
           </div>
 
           <div class="col-xs-12 col-sm-12 col-md-6">
-            <div class="Top Product">
-              <div class="img" style="background-image: url('http://images.saddlebackleather.com/image/upload/w_1280,h_1280,c_fill/Mont_Blanc/SLC/Products/01-60-0007-TO-3.jpg')"></div>
-              <div class="row">
-                <div class="col-xs ProductCol">
-                  <h3 class="Name">6 Suitcase</h3>
-                </div>
-                <div class="col-xs PriceCol">
-                  <div class="Price">120$</div>
-                </div>
-              </div>
-            </div>
+            <Product
+              class="Top"
+              name="6 Suitcase"
+              rating="5"
+              price="120"
+              img="http://images.saddlebackleather.com/image/upload/w_1280,h_1280,c_fill/Mont_Blanc/SLC/Products/01-60-0007-TO-3.jpg">
+            </Product>
           </div>
-
         </div>
 
         <h1 class="Title">Business Wallets</h1>
         <div class="row">
 
           <div class="col-xs-12 col-sm-6 col-md-3">
-            <div class="Product">
-              <div class="img" style="background-image: url('https://tumi.scene7.com/is/image/Tumi/79303GRY_alt2?hei=330&wid=330&layer=2&opac=0&layer=3&opac=0')"></div>
-              <div class="row">
-                <div class="col-xs ProductCol">
-                  <h3 class="Name">1 Wallet</h3>
-                </div>
-                <div class="col-xs PriceCol">
-                  <div class="Price">120$</div>
-                </div>
-              </div>
-            </div>
+            <Product
+              name="1 Wallet"
+              rating="3"
+              price="120"
+              img="https://tumi.scene7.com/is/image/Tumi/79303GRY_alt2?hei=330&wid=330&layer=2&opac=0&layer=3&opac=0">
+            </Product>
           </div>
 
           <div class="col-xs-12 col-sm-6 col-md-3">
-            <div class="Product">
-              <div class="img" style="background-image: url('https://www.smithandcanova.co.uk/502-3132-home/card-holder.jpg')"></div>
-              <div class="row">
-                <div class="col-xs ProductCol">
-                  <h3 class="Name">2 Wallets</h3>
-                </div>
-                <div class="col-xs PriceCol">
-                  <div class="Price">88$</div>
-                </div>
-              </div>
-            </div>
+            <Product
+              name="2 Wallet"
+              rating="3"
+              price="88"
+              img="https://www.smithandcanova.co.uk/502-3132-home/card-holder.jpg">
+            </Product>
           </div>
 
           <div class="col-xs-12 col-sm-6 col-md-3">
-            <div class="Product">
-              <div class="img" style="background-image: url('http://www.luxuryartisan.com/2995-home_default/all-you-need-leather-card-holder-tan.jpg')"></div>
-              <div class="row">
-                <div class="col-xs ProductCol">
-                  <h3 class="Name">3 Wallets</h3>
-                </div>
-                <div class="col-xs PriceCol">
-                  <div class="Price">40$</div>
-                </div>
-              </div>
-            </div>
+            <Product
+              name="3 Wallet"
+              rating="4"
+              price="40"
+              img="http://www.luxuryartisan.com/2995-home_default/all-you-need-leather-card-holder-tan.jpg">
+            </Product>
           </div>
 
 
           <div class="col-xs-12 col-sm-6 col-md-3">
-            <div class="Product">
-              <div class="img" style="background-image: url('http://cdn2.bigcommerce.com/server500/cc58c/products/3303/images/6715/PI423502RED_wallet_201__62715.1366282854.1280.1280_SQ__29762.1462988229.560.850.jpg?c=2')"></div>
-              <div class="row">
-                <div class="col-xs ProductCol">
-                  <h3 class="Name">4 Wallets</h3>
-                </div>
-                <div class="col-xs PriceCol">
-                  <div class="Price">10$</div>
-                </div>
-              </div>
-            </div>
+            <Product
+              name="4 Wallet"
+              rating="5"
+              price="56"
+              img="http://cdn2.bigcommerce.com/server500/cc58c/products/3303/images/6715/PI423502RED_wallet_201__62715.1366282854.1280.1280_SQ__29762.1462988229.560.850.jpg?c=2">
+            </Product>
           </div>
 
           <div class="col-xs-12 col-sm-12 col-md-6">
-            <div class="Top Product">
-              <div class="img" style="background-image: url('https://ae01.alicdn.com/kf/HTB1y9MpKVXXXXbNXpXXq6xXFXXXM/New-Business-Men-s-Clutch-Bags-Split-Leather-Handbags-Double-Zipper-Men-Clutch-Wallet-Large-Capacity.jpg_640x640.jpg')"></div>
-              <div class="row">
-                <div class="col-xs ProductCol">
-                  <h3 class="Name">5 Wallets</h3>
-                </div>
-                <div class="col-xs PriceCol">
-                  <div class="Price">10$</div>
-                </div>
-              </div>
-            </div>
+            <Product
+              class="Top"
+              name="5 Wallet"
+              rating="5"
+              price="94"
+              img="https://ae01.alicdn.com/kf/HTB1y9MpKVXXXXbNXpXXq6xXFXXXM/New-Business-Men-s-Clutch-Bags-Split-Leather-Handbags-Double-Zipper-Men-Clutch-Wallet-Large-Capacity.jpg_640x640.jpg">
+            </Product>
           </div>
 
           <div class="col-xs-12 col-sm-12 col-md-6">
-            <div class="Top Product">
-              <div class="img" style="background-image: url('https://s-media-cache-ak0.pinimg.com/736x/b9/5a/06/b95a06ff4b269aefe1b15bac2eae0c21.jpg')"></div>
-              <div class="row">
-                <div class="col-xs ProductCol">
-                  <h3 class="Name">6 Wallets</h3>
-                </div>
-                <div class="col-xs PriceCol">
-                  <div class="Price">56$</div>
-                </div>
-              </div>
-            </div>
+            <Product
+              class="Top"
+              name="6 Wallet"
+              rating="5"
+              price="76"
+              img="https://s-media-cache-ak0.pinimg.com/736x/b9/5a/06/b95a06ff4b269aefe1b15bac2eae0c21.jpg">
+            </Product>
           </div>
-
         </div>
+
+
+        <!-- Via Loops -->
+         <!-- <div class="row">
+          <div
+            v-for="product in suitcases"
+            class="col-xs-12"
+            v-bind:class="{
+              'col-sm-12 col-md-6': product.isTop,
+              'col-sm-6 col-md-3': !product.isTop
+            }"
+            >
+            <Product
+              v-bind:class="{
+                Top: product.isTop
+              }"
+              v-bind:name="product.name"
+              price="63"
+              img="http://g03.a.alicdn.com/kf/HTB1SuMSKVXXXXcRaXXXq6xXFXXXz/2015-Business-font-b-Leather-b-font-font-b-Trolley-b-font-Luggage-Vintage-Suitcase-Board.jpg">
+            </Product>
+          </div>
+        </div>
+
+        <h1 class="Title">Business Wallets</h1>
+
+        <div class="row">
+          <div class="col-xs-12 col-sm-6 col-md-3" v-for="product in wallets">
+            <Product
+              v-bind:name="product.name"
+              price="63"
+              img="http://g03.a.alicdn.com/kf/HTB1SuMSKVXXXXcRaXXXq6xXFXXXz/2015-Business-font-b-Leather-b-font-font-b-Trolley-b-font-Luggage-Vintage-Suitcase-Board.jpg">
+            </Product>
+          </div>
+        </div> -->
+
+
       </div>
     </div>
 
@@ -202,42 +180,19 @@
     padding: 100px 0  50px 0;
   }
 
-  .Product {
-    margin-bottom: 15px;
-
-  }
-
-  .PriceCol {
-    text-align: right;
-  }
-
-  .Product .Name {
-    margin: 0;
-  }
-
   .ProductsSection .Title {
     text-align: center;
-    color: #006cb7;
+    color: #6f6f6f;
     padding-bottom: 30px;
+    letter-spacing: 4px;
   }
 
-  .Product .img {
-    width: 100%;
-    height: 250px;
-    background-color: #ccc;
-    margin-bottom: 10px;
-    background-size: cover;
-    border: 1px solid #ccc;
+  .ProductsSection .SectionTitle {
+    color: #006cb7;
+    font-weight: bold;
+    letter-spacing: 7px;
   }
 
-  .Product.Top .img {
-    width: 100%;
-    height: 500px;
-    background-color: #ccc;
-    margin-bottom: 10px;
-    background-size: cover;
-    border: 1px solid #ccc;
-  }
 
 
 </style>
@@ -246,7 +201,7 @@
 <script>
 import Header from '../_common/Header/Header';
 import HeaderSecondary from '../_common/HeaderSecondary/HeaderSecondary';
-
+import Product from '../_common/Product/Product';
 import FooterSecondary from '../_common/FooterSecondary/FooterSecondary';
 import Footer from '../_common/Footer/Footer';
 
@@ -254,9 +209,31 @@ export default {
   components: {
     Header,
     HeaderSecondary,
-
+    Product,
     FooterSecondary,
     Footer,
+  },
+  data: function() {
+    return {
+      suitcases: [
+        {
+          name: "hello",
+          isTop: true
+        },
+        {
+          name: "world"
+        }
+      ],
+      suitcasesTop: [
+
+      ],
+      wallets: [
+
+      ],
+      walletsTop: [
+
+      ]
+    }
   }
 }
 </script>
---
+++ src/_common/Product/Product.vue
@@ -0,0 +1,79 @@
+<template>
+
+  <div class="Product">
+    <div class="row">
+      <div class="col-xs ProductCol">
+        <h3 class="Name">{{name}}</h3>
+      </div>
+    </div>
+    <div class="img" v-bind:style="{backgroundImage: 'url(' + img + ')'}"></div>
+    <div class="row">
+      <div class="RatingCol">
+        <star-rating star-size=25 v-model="rating"></star-rating>
+      </div>
+      <div class="col-xs PriceCol">
+        <div class="Price">{{price}}$</div>
+      </div>
+    </div>
+  </div>
+
+
+</template>
+
+
+
+<style>
+
+
+  .Product {
+    margin-bottom: 15px;
+
+  }
+
+  .PriceCol {
+    text-align: right;
+  }
+
+  .Product .Name {
+    margin: 0;
+    color: #6f6f6f;
+  }
+
+  .Product .star-rating{
+    margin-left: 10px;
+  }
+
+  .rating-text {
+      display: none;
+    }
+
+  .Product .img {
+    width: 100%;
+    height: 250px;
+    background-color: #ccc;
+    margin-bottom: 10px;
+    background-size: cover;
+    border: 1px solid #ccc;
+  }
+
+  .Product.Top .img {
+    height: 500px;
+  }
+
+
+
+</style>
+
+
+<script>
+import StarRating from 'vue-star-rating'
+
+export default {
+  props: ['name', 'price', 'img', 'rating'],
+
+  components: {
+      StarRating
+    }
+
+}
+</script>
